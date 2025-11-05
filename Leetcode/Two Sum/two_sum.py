26efaf350ead030cf03e86910d318800a3f19d3a
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-11-05 13:21:10 +0300

    two sum problem

--- Leetcode/Two Sum/two_sum.py
+++ Leetcode/Two Sum/two_sum.py
@@ -1,15 +1,12 @@
 from typing import List
-
 class Solution:
-    def two_sum(self, nums:List[int], target: int) -> List[int]:
-        num_indicies = {}
-        for i, num in enumerate(nums):
+    def two_sum(self,nums:List[int], target:int)->List[int]:
+        hash_map = {}
+        for i,num in enumerate(nums):
             difference = target - num
-            if difference in num_indicies:
-                return[num_indicies[difference], i]
-            num_indicies[num] = i
-
+            if difference in hash_map:
+                return[hash_map[difference],i]
+            hash_map[num] = i    
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.two_sum([2, 7, 11, 15], 18))
-
+    print(solution.two_sum([2,7,11,15],22))
