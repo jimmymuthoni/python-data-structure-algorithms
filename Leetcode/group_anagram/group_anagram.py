49776b8a8fd9361328f4a6a6f61452b4edacaff3
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-12-11 11:44:55 +0300

    grouping anagrams solution

---
+++ Leetcode/group_anagram/group_anagram.py
@@ -0,0 +1,30 @@
+from typing import List
+class Solution:
+    def group_anagram(self, strs:List[str]) -> List[List[str]]:
+        if len(strs) == 0:
+            return [[""]]
+        
+        ans_map = {}
+        for s in strs:
+            count = [0] * 26       
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            
+            key_part = []
+            
+            for num in count:
+                key_part.append("#")
+                key_part.append(str(num))
+            key = "".join(key_part)
+
+            if key not in ans_map:
+                ans_map[key] = []
+            ans_map[key].append(s)
+
+        return list(ans_map.values())
+    
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.group_anagram(['eat','tea','tan','ate','nat','bat']))
+            
+        
